a45b91bb70bfc7683253c8a03ea89bc82f4374b9
virgo <w2008second@gmail.com>
2017-06-20 18:22:27 +0800

    [feat]add brower webp support

--- src/_lib/_browser/index.js
+++ src/_lib/_browser/index.js
@@ -1,1 +1,10 @@
 export * from 'detector';
+
+let image = new Image();
+let webpSupport = false;
+image.onload = image.onerror = () => {
+	webpSupport = image.height === 1;
+};
+image.src = 'data:image/webp;base64,UklGRiYAAABXRUJQVlA4IBoAAAAwAQCdASoBAAEAAAAMJaQAA3AA/v89WAAAAA==';
+
+export { webpSupport };
